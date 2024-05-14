8928dbf8c6d8e90e5c1d09d0418234d9e0fe2005
GERLY DANIEL ARTEAGA BERNAL <arteaga.bernal3@gmail.com>
2024-05-14 15:32:25 -0600

    hola

--- cliente/src/App.java
+++ cliente/src/App.java
@@ -1,5 +1,34 @@
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import las_interfaces.IUsuario;
+import las_interfaces.IUsuarioController;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args)  {
+       try {
+        IUsuarioController usuarioController = 
+           (IUsuarioController) Naming.lookup("rmi://localhost/UsuarioController");
+
+           IUsuario usuario = usuarioController.newInstance();
+           usuario.setNombre("GABRIEL ANTONIO MORALES GARCIA");
+           usuario.setDireccion("calle ixhuatlan #305 colonia centro");
+           usuario.setTelefono("9123843829");
+           usuario.setNumeroIdentificacion("s21111111");
+           int respuesta = usuarioController.add(usuario);
+            if(respuesta == IUsuarioController.ADD_EXITO){
+                System.out.println("usuario agregado con exito");
+            }else{
+                System.out.println("algo salio mal :c");
+            }
+
+
+
+    } catch (MalformedURLException | RemoteException | NotBoundException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+    }
     }
 }
